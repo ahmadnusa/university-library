dff94fe885298a030f740ffadd80a1115a4dda3b
ahmadnusa <ahmadnursaefullah79@gmail.com>
2025-02-10 11:17:44 +0700

    Add database configuration and user schema; implement Drizzle ORM for database interactions

---
+++ src/database/drizzle.ts
@@ -0,0 +1,13 @@
+// import config from "@/lib/config"
+// import { neon } from "@neondatabase/serverless"
+// import { drizzle } from "drizzle-orm/neon-http"
+
+// const sql = neon(config.env.databaseUrl)
+
+// export const db = drizzle({ client: sql })
+import { config } from "dotenv"
+import { drizzle } from "drizzle-orm/neon-http"
+
+config({ path: ".env.local" }) // or .env.local
+
+export const db = drizzle(process.env.DATABASE_URL!)
