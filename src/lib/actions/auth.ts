91abed35929153d9b0e44f69c5555bda5a931a6a
ahmadnusa <ahmadnursaefullah79@gmail.com>
2025-02-11 10:07:33 +0700

    Add Redis and Upstash workflow integration; implement rate limiting and error handling in authentication

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -5,11 +5,19 @@ import { db } from "@/database/drizzle"
 import { users } from "@/database/schema"
 import { hash } from "bcryptjs"
 import { eq } from "drizzle-orm"
+import { headers } from "next/headers"
+import ratelimit from "../rateLimit"
+import { redirect } from "next/navigation"
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">,
 ) => {
   const { email, password } = params
+
+  const ip = (await headers()).get("x-forwaded-for") || "127.0.0.1"
+  const { success } = await ratelimit.limit(ip)
+
+  if (!success) redirect("/too-fast")
 
   try {
     const result = await signIn("credentials", {
@@ -32,8 +40,10 @@ export const signInWithCredentials = async (
 export const signUp = async (params: AuthCredentials) => {
   const { email, fullName, password, universityCard, universityId } = params
 
-  // const ip = (await headers()).get("x-forwaded-for") || "127.0.0.1"
-  // const {success} = await ratelimit.limit(ip)
+  const ip = (await headers()).get("x-forwaded-for") || "127.0.0.1"
+  const { success } = await ratelimit.limit(ip)
+
+  if (!success) redirect("/too-fast")
 
   const existingUser = await db
     .select()
