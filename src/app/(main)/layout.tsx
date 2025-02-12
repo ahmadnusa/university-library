7ca92027f959b559744731a6a211d1489f2fd114
ahmadnusa <ahmadnursaefullah79@gmail.com>
2025-02-12 08:56:35 +0700

    Implement user activity tracking and onboarding email workflow; enhance email sending functionality

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,11 +2,36 @@ import { ReactNode } from "react"
 import Header from "@/components/Header"
 import { auth } from "@/auth"
 import { redirect } from "next/navigation"
+import { after } from "next/server"
+import { db } from "@/database/drizzle"
+import { users } from "@/database/schema"
+import { eq } from "drizzle-orm"
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth()
 
   if (!session) redirect("/signin")
+
+  after(async () => {
+    if (!session?.user?.id) return
+
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session.user.id))
+      .limit(1)
+
+    if (user.length === 0) return
+    const lastActivityDate = user[0]?.lastActivityDate
+    const today = new Date().toISOString().slice(0, 10)
+
+    if (lastActivityDate === today) return
+
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session?.user?.id))
+  })
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -8,6 +8,8 @@ import { eq } from "drizzle-orm"
 import { headers } from "next/headers"
 import ratelimit from "../rateLimit"
 import { redirect } from "next/navigation"
+import { workflowClient } from "../workflow"
+import config from "../config"
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">,
@@ -64,6 +66,14 @@ export const signUp = async (params: AuthCredentials) => {
       universityId,
       password: hashPassword,
       universityCard,
+    })
+
+    await workflowClient.trigger({
+      url: `${config.env.apiEndpoint}/api/workflows/onboarding`,
+      body: {
+        email,
+        fullName,
+      },
     })
 
     await signInWithCredentials({ email, password })
